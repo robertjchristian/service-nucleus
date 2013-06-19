6c566bda2ff19b28d583837dc4bc354e792bc8e0
jfranklin-ross <jfranklin-ross@liaison.com>
2013-06-19 13:34:00 -0700

    log4j2 with auditsyslog appender

--- service-framework/src/main/java/com/liaison/framework/audit/DefaultAuditStatement.java
+++ service-framework/src/main/java/com/liaison/framework/audit/DefaultAuditStatement.java
@@ -1,5 +1,23 @@
 package com.liaison.framework.audit;
 
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.AnnotationIntrospector;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
+
+
+/**
+ * General use implementation of AuditStatement
+ * 
+ * Apply to any logger configured with AuditStatement aware appender.
+ *
+ */
 public class DefaultAuditStatement implements AuditStatement {
 
     private final AuditStandardsRequirement requirement;
@@ -14,22 +32,51 @@ public class DefaultAuditStatement implements AuditStatement {
 
     @Override
     public String getMessage() {
-        return this.message;
+        return message;
     }
 
     @Override
     public Status getStatus() {
-        return this.status;
+        return status;
     }
 
     @Override
     public AuditStandardsRequirement getAuditStandardsRequirement() {
-        return this.getAuditStandardsRequirement();
+        return requirement;
     }
 
+    /**
+     * makes a JSON version of this audit statement.
+     */
     public String toString() {
-      return getAuditStandardsRequirement().toString() + getAuditStandardsRequirement().getDescription() + "::" + getStatus() + "::" + getMessage();
+    	try {
+    		return marshallToJson(this);
+		} catch (  JAXBException| IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return message;
     }
+    
+    //TODO move to jaxbutil
+	@SuppressWarnings("deprecation")
+	private String marshallToJson(AuditStatement object) throws JAXBException,
+		JsonGenerationException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		AnnotationIntrospector primary = new JaxbAnnotationIntrospector();
+		AnnotationIntrospector secondary = new JacksonAnnotationIntrospector();
+		AnnotationIntrospector introspector = new AnnotationIntrospector.Pair(
+				primary, secondary);
+
+		// make deserializer use JAXB annotations (only)
+		mapper.getDeserializationConfig().setAnnotationIntrospector(
+				introspector);
+		// make serializer use JAXB annotations (only)
+		mapper.getSerializationConfig().setAnnotationIntrospector(introspector);
+
+		return mapper.writeValueAsString(object);
+	}
+
 
 
 }
