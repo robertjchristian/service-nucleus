37240f323530183b0dd0edd7ae1d3c6c36d87821
Robert Christian <robertjchristian@gmail.com>
2013-08-24 12:35:38 -0700

    added support for form validation and binding, carousel, and codec

--- service-implementation/src/main/webapp/playground/playground.js
+++ service-implementation/src/main/webapp/playground/playground.js
@@ -4,15 +4,15 @@
 function createDataGrid($scope, $http, dataUrl) {
 
     $scope.filterOptions = {
-        filterText:"",
-        useExternalFilter:true
+        filterText: "",
+        useExternalFilter: true
     };
 
     $scope.pagingOptions = {
-        pageSizes:[10, 25, 100],
-        pageSize:10,
-        totalServerItems:15,
-        currentPage:1
+        pageSizes: [10, 25, 100],
+        pageSize: 10,
+        totalServerItems: 15,
+        currentPage: 1
     };
 
     $scope.getPagedDataAsync = function (pageSize, page, searchText) {
@@ -28,10 +28,10 @@ function createDataGrid($scope, $http, dataUrl) {
                         function (servicesJson) {
                             data = servicesJson.filter(function (item) {
                                 return JSON.stringify(
-                                    item)
+                                        item)
                                     .toLowerCase()
                                     .indexOf(
-                                    ft) != -1;
+                                        ft) != -1;
                             });
                             $scope.setPagingData(
                                 data,
@@ -80,166 +80,226 @@ function createDataGrid($scope, $http, dataUrl) {
     $scope.url = "";
 
     $scope.gridOptions = {
-        canSelectRows:true,
-        multiSelect:false,
-        jqueryUITheme:true,
-        displaySelectionCheckbox:false,
-        data:'servicesData',
-        selectedItems:$scope.mySelections,
-        enablePaging:true,
-        pagingOptions:$scope.pagingOptions,
-        filterOptions:$scope.filterOptions
+        canSelectRows: true,
+        multiSelect: false,
+        jqueryUITheme: true,
+        displaySelectionCheckbox: false,
+        data: 'servicesData',
+        selectedItems: $scope.mySelections,
+        enablePaging: true,
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions
     };
 }
 
 var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
-    // add service name to the scope...
-    $scope.widgetName = $routeParams.widgetName;
-    $scope.widgetUrl = "playground/" + $routeParams.widgetName + ".html"
 
-    //console.log("Playground controller::widgetName::" + $scope.widgetName);
-    //console.log("Playground controller::widgetUrl::" + $scope.widgetUrl);
-
-    // tree support
-    $scope.deleteNode = function (data) {
-        data.nodes = [];
-    };
-    $scope.addNode = function (data) {
-        var post = data.nodes.length + 1;
-        var newName = data.name + '-' + post;
-        data.nodes.push({name:newName, nodes:[]});
-    };
-    $scope.tree = [
-        {name:"Node", nodes:[]}
-    ];
+        // add service name to the scope...
+        $scope.widgetName = $routeParams.widgetName;
+        $scope.widgetUrl = "playground/" + $routeParams.widgetName + ".html"
 
 
-    // data grid
-    createDataGrid($scope, $http, 'playground/emp.json');
+        // tree support
+        $scope.deleteNode = function (data) {
+            data.nodes = [];
+        };
+        $scope.addNode = function (data) {
+            var post = data.nodes.length + 1;
+            var newName = data.name + '-' + post;
+            data.nodes.push({name: newName, nodes: []});
+        };
+        $scope.tree = [
+            {name: "Node", nodes: []}
+        ];
 
-    //tabs
-    $scope.panes = [
-        { title:"Dynamic Title 1", content:"Dynamic content 1" },
-        { title:"Dynamic Title 2", content:"Dynamic content 2" }
-    ];
 
-    // pagination
-    $scope.noOfPages = 15;
-    $scope.currentPage = 2;
+        // data grid
+        createDataGrid($scope, $http, 'playground/emp.json');
+
+        //tabs
+        $scope.panes = [
+            { title: "Dynamic Title 1", content: "Dynamic content 1" },
+            { title: "Dynamic Title 2", content: "Dynamic content 2" }
+        ];
+
+        // pagination
+        $scope.noOfPages = 15;
+        $scope.currentPage = 2;
 
 
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
 
-    // modal
-    $scope.open = function () {
-        $scope.shouldBeOpen = true;
-    };
+        // modal
+        $scope.open = function () {
+            $scope.shouldBeOpen = true;
+        };
 
-    $scope.close = function () {
-        $scope.closeMsg = 'I was closed at: ' + new Date();
-        $scope.shouldBeOpen = false;
-    };
+        $scope.close = function () {
+            $scope.closeMsg = 'I was closed at: ' + new Date();
+            $scope.shouldBeOpen = false;
+        };
 
-    // date
-    $scope.date2 = null;
+        // date
+        $scope.date2 = null;
 
-    $scope.addChild = function () {
-        $scope.events.push({
-            title:'Open Sesame',
-            start:new Date(y, m, 28),
-            end:new Date(y, m, 29)
-        });
-    }
+        $scope.addChild = function () {
+            $scope.events.push({
+                title: 'Open Sesame',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29)
+            });
+        }
 
-    $scope.remove = function (index) {
-        $scope.events.splice(index, 1);
-    }
+        $scope.remove = function (index) {
+            $scope.events.splice(index, 1);
+        }
 
-    // alerts (angular ui)
-    $scope.alertSet = [
-        { type:'error', msg:'Oh snap! Change a few things up and try submitting again.' },
-        { type:'success', msg:'Well done! You successfully read this important alert message.' }
-    ];
+        // alerts (angular ui)
+        $scope.alertSet = [
+            { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' },
+            { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+        ];
 
-    $scope.addToAlertSet = function () {
-        $scope.alertSet.push({msg:"Another alert!"});
-    };
+        $scope.addToAlertSet = function () {
+            $scope.alertSet.push({msg: "Another alert!"});
+        };
 
-    $scope.closeTheAlert = function (index) {
-        $scope.alertSet.splice(index, 1);
-    };
+        $scope.closeTheAlert = function (index) {
+            $scope.alertSet.splice(index, 1);
+        };
 
-    // angularstrap
-    $scope.modal2 = {content:'Hello Modal', saved:false};
-    $scope.tooltip = {title:"Hello Tooltip<br />This is a multiline message!", checked:false};
-    $scope.popover = {content:"Hello Popover<br />This is a multiline message!", saved:false};
-    $scope.alerts = [
-        {type:'success', title:'Hello!', content:'This is a success msg.<br><br><pre>2 + 3 = {{ 2 + 3 }}</pre>'}
-    ];
-    $scope.addAlert = function (type) {
-        $scope.alerts.push({type:type, title:'Alert!', content:'This is another alert...'});
-    };
-    $scope.button = {active:true};
-    $scope.buttonSelect = {price:'89,99', currency:'â‚¬'};
-    $scope.checkbox = {left:false, middle:true, right:false};
-    $scope.radio = {left:false, middle:true, right:false};
-    $scope.radioValue = 'middle';
-    $scope.typeahead = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-    $scope.datepicker = {dateStrap:'12/12/2012'};
-    $scope.timepicker = {timeStrap:'05:30 PM'};
+        // angularstrap
+        $scope.modal2 = {content: 'Hello Modal', saved: false};
+        $scope.tooltip = {title: "Hello Tooltip<br />This is a multiline message!", checked: false};
+        $scope.popover = {content: "Hello Popover<br />This is a multiline message!", saved: false};
+        $scope.alerts = [
+            {type: 'success', title: 'Hello!', content: 'This is a success msg.<br><br><pre>2 + 3 = {{ 2 + 3 }}</pre>'}
+        ];
+        $scope.addAlert = function (type) {
+            $scope.alerts.push({type: type, title: 'Alert!', content: 'This is another alert...'});
+        };
+        $scope.button = {active: true};
+        $scope.buttonSelect = {price: '89,99', currency: 'â‚¬'};
+        $scope.checkbox = {left: false, middle: true, right: false};
+        $scope.radio = {left: false, middle: true, right: false};
+        $scope.radioValue = 'middle';
+        $scope.typeahead = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+        $scope.datepicker = {dateStrap: '12/12/2012'};
+        $scope.timepicker = {timeStrap: '05:30 PM'};
 
-    $scope.prettyPrint = function () {
-        window.prettyPrint && window.prettyPrint();
-    }
+        $scope.prettyPrint = function () {
+            window.prettyPrint && window.prettyPrint();
+        }
 
-    // ace (for more options see http://ace.c9.io/#nav=howto)
-    $scope.aceIDEs =
-        {'JSON':  'var hw = new function() {\n  console.log("Hello world!");\n}',
-        'HTML':  '<!DOCTYPE html>\n<html>\n  <head>\n  </head>\n  <body>\n    <h1>Hello</h1>\n    <h2>World!</h2>\n  </body>\n</html>',
-        'XML':  '‹?xml version= “1.0”›\n<x key="value">\n  <y>\n   Hello World!\n  </y>\n</x>',
-        'Java': 'package com.foo.Hello;\n\npublic class HelloWorld {\n\n  public static void main(String args[]) {\n    System.out.println("Hello World!");\n  }\n\n}',
-        'Javascript': '[\n  {\n    "w1": "Hello"\n  },\n  {\n    "w2": "World"\n  }\n]',
-        'Python': 'print("Hello, World!")'
-    };
+        // ace (for more options see http://ace.c9.io/#nav=howto)
+        $scope.aceIDEs =
+        {'JSON': 'var hw = new function() {\n  console.log("Hello world!");\n}',
+            'HTML': '<!DOCTYPE html>\n<html>\n  <head>\n  </head>\n  <body>\n    <h1>Hello</h1>\n    <h2>World!</h2>\n  </body>\n</html>',
+            'XML': '‹?xml version= “1.0”›\n<x key="value">\n  <y>\n   Hello World!\n  </y>\n</x>',
+            'Java': 'package com.foo.Hello;\n\npublic class HelloWorld {\n\n  public static void main(String args[]) {\n    System.out.println("Hello World!");\n  }\n\n}',
+            'Javascript': '[\n  {\n    "w1": "Hello"\n  },\n  {\n    "w2": "World"\n  }\n]',
+            'Python': 'print("Hello, World!")'
+        };
 
-    $scope.loadAceJSExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['Javascript']);
-        _editor.getSession().setUseWorker(false);
+        $scope.loadAceJSExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['Javascript']);
+            _editor.getSession().setUseWorker(false);
 
-    };
+        };
 
-    $scope.aceJSExampleChanged = function(e, acee) {
+        $scope.aceJSExampleChanged = function (e, acee) {
 
-       // TODO Not sure how JavaScript validation works.
-       // Have an open Google Groups question on this
-       //console.log("e: " + e + ", acee: " + acee);
+            // TODO Not sure how JavaScript validation works.
+            // Have an open Google Groups question on this
+            //console.log("e: " + e + ", acee: " + acee);
 
-    };
-    $scope.loadAceHTMLExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['HTML']);
-        _editor.getSession().setUseWorker(false);
-    };
+        };
+        $scope.loadAceHTMLExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['HTML']);
+            _editor.getSession().setUseWorker(false);
+        };
 
-    $scope.loadAceXMLExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['XML']);
-        _editor.getSession().setUseWorker(false);
-    };
+        $scope.loadAceXMLExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['XML']);
+            _editor.getSession().setUseWorker(false);
+        };
 
-    $scope.loadAceJSONExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['JSON']);
-        _editor.getSession().setUseWorker(false);
-    };
+        $scope.loadAceJSONExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['JSON']);
+            _editor.getSession().setUseWorker(false);
+        };
 
-    $scope.loadAceJavaExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['Java']);
-        _editor.getSession().setUseWorker(false);
-    };
+        $scope.loadAceJavaExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['Java']);
+            _editor.getSession().setUseWorker(false);
+        };
 
-    $scope.loadAcePythonExample = function(_editor){
-        _editor.setValue($scope.aceIDEs['Python']);
-        _editor.getSession().setUseWorker(false);
-    };
+        $scope.loadAcePythonExample = function (_editor) {
+            _editor.setValue($scope.aceIDEs['Python']);
+            _editor.getSession().setUseWorker(false);
+        };
 
-}]);
+
+        // TODO this is the wrong way to implement the service (client and server)...
+        // should be put in request body instead of on URI... otherwise we run into
+        // issue with length of uri, legibility of uri, encoding, etc
+
+        $scope.codecOutput = "";
+        $scope.codecError = false;
+
+        // TODO this should all be moved to a service
+        $scope.codecService = function (func, input, callback) {
+            console.log(func + ": " + input);
+            input = encodeURIComponent(input);
+            var url = '/hello-world/rest/v1/codec/' + func + '/' + input;
+            return $http({method: 'GET', url: url})
+                .success(function (data, status, headers, config) {
+                    callback(data, true);
+                })
+                .error(function (data, status, headers, config) {
+                    //console.log("failed to retrieve data");
+                    callback(data, false);
+                });
+        };
+
+        $scope.encode = function (input) {
+            $scope.codecService('encode', input, function (data, success) {
+                    console.log(data);
+                    $scope.codecOutput = data;
+                    $scope.codecError = !success;
+                }
+            );
+        }
+
+        $scope.decode = function (input) {
+            $scope.codecService('decode', input, function (data) {
+                    console.log(data);
+                    $scope.codecOutput = data;
+                }
+            );
+        }
+
+
+        // form validation and binding
+        $scope.master = "";
+
+        $scope.saveForm = function (user) {
+            console.log("User..." + $scope.user);
+            $scope.master = user;
+        }
+
+
+
+        // carousel
+        $scope.slides = [
+            {"image": "http://cdn-static.zdnet.com/i/r/story/70/00/004209/original/raspberry-pi-supercomputer-1-620x465.jpg?hash=AQx4MwRjZG", "text": "foo", "active": true},
+            {"image": "http://www.rubberrepublic.com/wp-content/uploads/2011/09/lolcat-funny-picture-moderator1.jpg", "text": "bar", "active": false},
+            {"image": "http://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Composition_of_38th_Parliament.png/220px-Composition_of_38th_Parliament.png", "text": "moo", "active": false}
+        ]
+
+
+
+    }])
+    ;
