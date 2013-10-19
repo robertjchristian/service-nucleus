3a311775adcc8c7dc0e507b3978f26fe9a1e89c2
Robert Christian <rchristian@liaison.com>
2013-10-19 13:05:52 -0700

    widget fixes (bootsrap 3 incompatibilities)

--- service-implementation/src/main/webapp/playground/playground.js
+++ service-implementation/src/main/webapp/playground/playground.js
@@ -119,29 +119,8 @@ var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '
         // data grid
         createDataGrid($scope, $http, 'playground/emp.json');
 
-        //tabs
-        $scope.panes = [
-            { title: "Dynamic Title 1", content: "Dynamic content 1" },
-            { title: "Dynamic Title 2", content: "Dynamic content 2" }
-        ];
-
-        // pagination
-        $scope.noOfPages = 15;
-        $scope.currentPage = 2;
-
-
         $scope.setPage = function (pageNo) {
             $scope.currentPage = pageNo;
-        };
-
-        // modal
-        $scope.open = function () {
-            $scope.shouldBeOpen = true;
-        };
-
-        $scope.close = function () {
-            $scope.closeMsg = 'I was closed at: ' + new Date();
-            $scope.shouldBeOpen = false;
         };
 
         // date
