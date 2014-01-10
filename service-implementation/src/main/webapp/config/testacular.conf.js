dabf8fb686a8068aed25409f8a6704e4b66edee5
Robert Christian <rchristian@liaison.com>
2014-01-09 17:06:44 -0800

    ported Jukka's work on testacular test config

--- service-implementation/src/main/webapp/config/testacular.conf.js
+++ service-implementation/src/main/webapp/config/testacular.conf.js
@@ -1,20 +1,29 @@
-basePath = '../';
-
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'app/lib/angular/angular.js',
-    'app/lib/angular/angular-*.js',
-    'test/lib/angular/angular-mocks.js',
-    'app/js/**/*.js',
-    'test/unit/**/*.js'
-];
-
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-    outputFile:'test_out/unit.xml',
-    suite:'unit'
+module.exports = function(config) {
+    config.set({
+        // your config
+        basePath: '../',
+        frameworks: ["jasmine"],
+        browsers: ["Chrome"],
+        files: [
+            'ui/js/vendor/angular-1.2.0-rc.3/angular.js',
+            'ui/js/vendor/angular-1.2.0-rc.3/angular-*.js',
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js',
+            'ui/js/vendor/angularui/*.js',
+            'ui/js/vendor/ace/*.js',
+            'http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.6.0.js',
+            'ui/js/vendor/bootstrap/*.js',
+            'ui/js/vendor/ng-grid/*.js',
+            'ui/js/vendor/angularstrap/*.js',
+            'ui/js/*.js',
+            'ui/js/services/*.js',
+            'ui/js/controllers/**/*.js',
+            'ui/js/directives/**/*.js',
+            'ui/js/filters/*.js',
+            'test/unit/**/*.js'
+        ],
+        exclude: [
+            'ui/js/vendor/angular-1.2.0-rc.3/angular-scenario.js'
+        ]
+    });
 };
